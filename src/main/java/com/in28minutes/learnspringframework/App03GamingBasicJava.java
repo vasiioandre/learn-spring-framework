0d8276a8aa68942cb08e43252c0d8bc590c886bd
Ioana Andreea Vasi <Ioana-Andreea.Vasi@endava.com>
2024-01-10 22:47:49 +0200

    Getting started with Java Spring Framework

---
+++ src/main/java/com/in28minutes/learnspringframework/App03GamingBasicJava.java
@@ -0,0 +1,22 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+import com.in28minutes.learnspringframework.game.PacManGame;
+
+public class App03GamingBasicJava {
+
+	public static void main(String[] args) {
+
+		try (var context = 
+				new AnnotationConfigApplicationContext
+					(GamingConfiguration.class)) {
+
+			context.getBean(GamingConsole.class).up();
+			
+			context.getBean(GameRunner.class).run();
+		}
+	}
+}
---
+++ src/main/java/com/in28minutes/learnspringframework/helloworld/HelloWorldConfiguration.java
@@ -0,0 +1,62 @@
+package com.in28minutes.learnspringframework.helloworld;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+record Person (String name, int age, Address address) { };
+record Address (String street, String city) { };
+
+@Configuration
+public class HelloWorldConfiguration {
+	
+	@Bean
+	public String name() {
+		return "Ranga";
+	}
+	
+	@Bean
+	public int age() {
+		return 15;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Ravi", 20, new Address("Baker Street", "London"));
+	}
+	
+	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address());
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address address2) {
+		return new Person(name, age, address2);
+	}
+	
+	@Bean
+	@Primary
+	public Person person4Parameters(String name, int age, Address address) {
+		return new Person(name, age, address);
+	}
+	
+	@Bean
+	public Person person5Qualifier(String name, int age, @Qualifier("address3qualifier") Address address) {
+		return new Person(name, age, address);
+	}
+	
+	@Bean(name = "address2")
+	@Primary
+	public Address address() {
+		return new Address("Versului", "Timisoara");
+	}
+	
+	@Bean(name = "address3")
+	@Qualifier("address3qualifier")
+	public Address address3() {
+		return new Address("Versului", "Timisoara");
+	}
+
+}
